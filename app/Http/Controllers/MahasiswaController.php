c54a281dfdcd721d8f45d482703352cb3d74b3aa
larven313 <hidayatullahsukma@gmail.com>
2022-12-25 01:20:20 +0700

    buat read mahasiswa

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // get all data mahasiswa join to tbl_prodi
+        $students = DB::table('tbl_mahasiswa')
+            ->join('tbl_prodi', 'tbl_prodi.id', 'tbl_mahasiswa.prodi_kode')
+            ->select('tbl_mahasiswa.*', 'tbl_prodi.kode as prodi')
+            ->get();
+
+        return view('mahasiswa.index', [
+            'students' => $students
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Mahasiswa  $mahasiswa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Mahasiswa $mahasiswa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Mahasiswa  $mahasiswa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Mahasiswa $mahasiswa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Mahasiswa  $mahasiswa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Mahasiswa $mahasiswa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Mahasiswa  $mahasiswa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Mahasiswa $mahasiswa)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
+use App\Models\Mahasiswa;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +20,6 @@ Route::get('/', function () {
 });
 
 Route::resource('products', ProductController::class);
+
+// Route Mahasiswa
+Route::get('mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa');
